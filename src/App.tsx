47e9e4332c19224293290d2374851a60c5fc6c7f
Idan Levian <41434778+eidan66@users.noreply.github.com>
2024-10-22 20:33:12 +0300

    Adding Perfect team generator

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import WeakerPlayer from './screens/WeakerPlayer';
 import NormalPlayer from './screens/NormalPlayer';
 import ExcellentPlayer from './screens/ExcellentPlayer';
+import PerfectPlayer from './screens/PerfectPlayer';
 import './App.css';
 
 const App: React.FC = () => {
@@ -13,12 +14,14 @@ const App: React.FC = () => {
       <Link to="/weaker"><button className="styled-button">Generate Weaker Players</button></Link>
       <Link to="/normal"><button className="styled-button">Generate Normal Players</button></Link>
       <Link to="/excellent"><button className="styled-button">Generate Excellent Players</button></Link>
+      <Link to="/perfect"><button className="styled-button">Generate Perfect Team</button></Link>
 
 
         <Routes>
           <Route path="/weaker" element={<WeakerPlayer />} />
           <Route path="/normal" element={<NormalPlayer />} />
           <Route path="/excellent" element={<ExcellentPlayer />} />
+          <Route path="/perfect" element={<PerfectPlayer />} />
           <Route path="/" element={<h1>Welcome! Select a player level to view</h1>} />
         </Routes>
       </div>
--- src/api/index.ts
+++ src/api/index.ts
@@ -2,5 +2,5 @@ export const BASE_URL = true ? 'https://soccer-simulation.onrender.com/api' : 'h
 
 export const BASE_TEAM_URL = `${BASE_URL}/team`
 export const TEAM_WEAKER = `${BASE_TEAM_URL}/weaker`
-// export const TEAM_NORMAL = `${BASE_TEAM_URL}/normal`
+export const TEAM_NORMAL = `${BASE_TEAM_URL}/normal`
 export const TEAM_EXCELLENT = `${BASE_TEAM_URL}/excellent`
